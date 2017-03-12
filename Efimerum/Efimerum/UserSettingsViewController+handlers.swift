41833774b83d888487f7a2b4094749a0c1358be6
Charles Moncada <charles.moncada@emoshiapps.com>
2017-03-12 17:00:28 -0500

    Settings scene switches ready to do:
    implement all handlers

---
+++ Efimerum/Efimerum/UserSettingsViewController+handlers.swift
@@ -0,0 +1,33 @@
+//
+//  UserSettingsViewController+handlers.swift
+//  Efimerum
+//
+//  Created by Charles Moncada on 12/03/17.
+//  Copyright © 2017 mibarbou. All rights reserved.
+//
+
+import UIKit
+
+extension UserSettingsViewController {
+    
+    func handleLogout() {
+        print("me deberia desloguear")
+    }
+    
+    func handleModifyUser() {
+        print("deberia modificar datos de usuario")
+    }
+    
+    func handleLikeNotificationSwitchChanged(sender: UISwitch) {
+        print("movieron el switch a: \(sender.isOn)")
+    }
+    
+    func handleFavoritePostSwitchChanged(sender: UISwitch) {
+        print("movieron el switch a: \(sender.isOn)")
+    }
+    
+    func handleAdultContentSwitchChanged(sender: UISwitch) {
+        print("movieron el switch a: \(sender.isOn)")
+    }
+    
+}
